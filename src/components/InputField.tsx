432bec6b3e8b3ffc538a32663ccfbdc0d6bd851e
ararita <munjizavesna@gmail.com>
2022-01-29 22:19:48 +0100

    Add todo component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,16 +3,36 @@
 // }
 
 // export default InputField;
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div>
-      <form className="input" action="">
-        <input type="text" placeholder="Enter a task" className="input__box" />
+      <form
+        className="input"
+        onSubmit={(e) => {
+          handleAdd(e);
+          inputRef.current?.blur();
+        }}
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Enter a task"
+          className="input__box"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
         <button type="submit" className="input__submit">
-          Go
+          Add
         </button>
       </form>
     </div>
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Todo } from "../model";
+
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  return (
+    <form action="" className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+    </form>
+  );
+};
+
+export default SingleTodo;
